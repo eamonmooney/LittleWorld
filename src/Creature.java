ec3a34eb409a7fe4586499852c8ccc34244f2cbb
eamonmooney <eamonmooney.ea@gmail.com>
2026-04-03 02:24:57 +0100

    World border

--- src/Creature.java
+++ src/Creature.java
@@ -3,15 +3,26 @@ import java.util.Random;
 public class Creature {
     public double x, y;
     private static final Random random = new Random();
+    private double worldWidth, worldHeight;
 
-    public Creature(double x, double y) {
+    public Creature(double x, double y, double worldWidth, double worldHeight) {
         this.x = x;
         this.y = y;
+        this.worldWidth = worldWidth;
+        this.worldHeight = worldHeight;
     }
 
     public void move() {
         // Move randomly by -10, 0, or 10 in both x and y directions
-        this.x += (random.nextInt(3) - 1) * 10;
-        this.y += (random.nextInt(3) - 1) * 10;
+        double newX = this.x + (random.nextInt(3) - 1) * 10;
+        double newY = this.y + (random.nextInt(3) - 1) * 10;
+
+        // Ensure the new position is within bounds
+        if (newX >= 0 && newX <= worldWidth) {
+            this.x = newX;
+        }
+        if (newY >= 0 && newY <= worldHeight) {
+            this.y = newY;
+        }
     }
 }
--- src/World.java
+++ src/World.java
@@ -3,13 +3,15 @@ import java.util.List;
 
 public class World {
     private List<Creature> creatures;
+    private static final double WORLD_WIDTH = 600; // Match the scene width
+    private static final double WORLD_HEIGHT = 400; // Match the scene height
 
     public World() {
         creatures = new ArrayList<>();
         // spawn a few creatures at random positions
-        creatures.add(new Creature(100, 100));
-        creatures.add(new Creature(200, 150));
-        creatures.add(new Creature(300, 200));
+        creatures.add(new Creature(100, 100, WORLD_WIDTH, WORLD_HEIGHT));
+        creatures.add(new Creature(200, 150, WORLD_WIDTH, WORLD_HEIGHT));
+        creatures.add(new Creature(300, 200, WORLD_WIDTH, WORLD_HEIGHT));
     }
 
     public List<Creature> getCreatures() {
